24c533148f3eca44eeff48d999f378b39a6b3f30
Almir <contact.almir@gmail.com>
2020-12-01 02:32:11 -0600

    Update redirect back to homepage

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,20 +1,7 @@
-import React, { useEffect } from 'react';
-import { navigate } from 'gatsby';
-
-// import Layout from '../components/layout';
-// import SEO from '../components/seo';
-
 export default function NotFoundPage() {
-	useEffect(() => {
-		navigate('/');
-	}, []);
-	return (
-		<div>
-			<h1>404: Not Found</h1>
-			<p>
-				You just hit a route that doesn&#39;t exist... the
-				sadness.
-			</p>
-		</div>
-	);
+	if (typeof window !== 'undefined') {
+		window.location = '/';
+	}
+
+	return null;
 }
