f4e8f8242e3c1c9e2e02473b2fc90c92ee9f9f1a
Almir <contact.almir@gmail.com>
2020-12-01 01:27:23 -0600

    Setup config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,34 @@
  */
 
 module.exports = {
-  /* Your site config here */
-  plugins: [],
-}
+	/* Your site config here */
+	siteMetadata: {
+		title: `Almir`,
+		description: `Almir - Web Developer and User Interface Designer`,
+		author: `Almir`
+	},
+	plugins: [
+		`gatsby-plugin-react-helmet`,
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `images`,
+				path: `${__dirname}/src/images`
+			}
+		},
+		`gatsby-transformer-sharp`,
+		`gatsby-plugin-sharp`,
+		{
+			resolve: `gatsby-plugin-manifest`,
+			options: {
+				name: `gatsby-starter-default`,
+				short_name: `Almir`,
+				start_url: `/`,
+				background_color: `#14181c`,
+				theme_color: `#14181c`,
+				display: `minimal-ui`,
+				icon: `src/images/AM-logo.svg` // This path is relative to the root of the site.
+			}
+		}
+	]
+};
