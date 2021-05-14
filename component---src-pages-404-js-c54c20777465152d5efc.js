66f24c2ea87a7d9fb75176db3101d17fa2fd636e
Almir <contact.almir@gmail.com>
2021-05-14 00:47:39 -0500

    Delete old portfolio code

--- component---src-pages-404-js-c54c20777465152d5efc.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{w2l6:function(t,e,n){"use strict";n.r(e),n.d(e,"default",(function(){return o}));var u=n("q1tI"),a=n.n(u),c=n("Wbzz");function o(){return Object(u.useEffect)((function(){Object(c.navigate)("/")}),[]),a.a.createElement("div",null,a.a.createElement("h1",null,"404: Not Found"),a.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-c54c20777465152d5efc.js.map
